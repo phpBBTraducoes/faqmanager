994004ac8ea0395b93613d11804b118db6209709
eunaumtenhoid <eunaumtenhoid@outlook.com>
2018-08-26 09:39:28 -0300

    Traducao ADD

---
+++ language/pt_br/info_acp_faqmanager.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * FAQ manager reloaded. An extension for the phpBB Forum Software package.
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2018 [ver 1.0.0-beta] (https://github.com/phpBBTraducoes)
+ * @copyright (c) 2018, Ger, https://github.com/gerb
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+    'FM_ANSWER'                     => 'Resposta',
+    'FM_CAT_ADD'                    => 'Adicionar categoia',
+    'FM_CAT_DELETED'                => 'Categoria com crianças deletadas',
+    'FM_CAT_TITLE'                  => 'Título da categoria',
+    'FM_CHILDREN_WILL_BE_DELETED'	=> 'Todas as entradas nesta categoria também serão deletadas!',
+    'FM_DEFAULTS_IMPORT'            => 'Ler arquivos de idioma padrão no banco de dados FAQ',
+    'FM_DEFAULTS_EXPLAIN'           => 'Se você continuar, todas as entradas de FAQ existentes serão removidas do banco de dados. O arquivo padrão <code>./language/**/help/faq.php</code> será importado para cada idioma ativado.<br>',
+	'FM_FAQ_MANAGER'                => 'Gerenciar FAQ',
+	'FM_FAQ_DELETED'                => 'Entrada de FAQ deletada',
+	'FM_LANG'                       => 'Idioma para esta categoria e suas entradas',
+	'FM_QUESTION'                   => 'Questão',
+	'FM_QUESTION_ADD'               => 'Adicionar nova questão a esta categoria',
+));

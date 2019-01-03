88d190c83565f1b17ff7dbb18c345ea4e8fa7275
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-01-03 12:06:50 -0200

    UPDATE

--- language/pt_br/common.php
+++ language/pt_br/common.php
@@ -2,7 +2,7 @@
 /**
  *
  * FAQ manager reloaded. An extension for the phpBB Forum Software package.
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2018 [ver 1.0.0-beta] (https://github.com/phpBBTraducoes)
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2018 [ver 1.0.0-b7] (https://github.com/phpBBTraducoes)
  * @copyright (c) 2018, Ger, https://github.com/gerb
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
--- language/pt_br/info_acp_faqmanager.php
+++ language/pt_br/info_acp_faqmanager.php
@@ -2,7 +2,7 @@
 /**
  *
  * FAQ manager reloaded. An extension for the phpBB Forum Software package.
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2018 [ver 1.0.0-beta] (https://github.com/phpBBTraducoes)
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2018 [ver 1.0.0-b7] (https://github.com/phpBBTraducoes)
  * @copyright (c) 2018, Ger, https://github.com/gerb
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
@@ -19,7 +19,8 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-    'FM_ANSWER'                     => 'Resposta',
+    'FM_ACP_SAVED'                  => 'FAQ saved'
+	'FM_ANSWER'                     => 'Resposta',
     'FM_CAT_ADD'                    => 'Adicionar categoia',
     'FM_CAT_DELETED'                => 'Categoria com crianças deletadas',
     'FM_CAT_TITLE'                  => 'Título da categoria',
@@ -29,6 +30,7 @@ $lang = array_merge($lang, array(
 	'FM_FAQ_MANAGER'                => 'Gerenciar FAQ',
 	'FM_FAQ_DELETED'                => 'Entrada de FAQ deletada',
 	'FM_LANG'                       => 'Idioma para esta categoria e suas entradas',
+	'FM_NO_CATEGORIES'              => 'Nenhuma categoria encontrada',
 	'FM_QUESTION'                   => 'Questão',
 	'FM_QUESTION_ADD'               => 'Adicionar nova questão a esta categoria',
 ));
